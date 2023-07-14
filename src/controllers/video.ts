7fdb44e9968997662e53a153134ceb0b461f5948
Ed Soto <edsghot@gmail.com>
2023-07-14 17:42:40 -0500

    feat: agregando insert al controlador de video

--- src/controllers/video.ts
+++ src/controllers/video.ts
@@ -14,3 +14,20 @@ export const getallVideo = async(req:Request,res:Response)=>{
         res.status(500).json({ message: "Error retrieving video", error: error });
       }
 } 
+
+export const insertVideo = async (req:Request, res:Response) => {
+    try {
+      const { name, description, url } = req.body;
+      const download = false;
+      const state = false;
+      if (!name || !description || !url) {
+        return res.status(400).json({ msg: "uno o mas campos vacios" });
+      }
+      const video = await Video.create({ name, description, url,download,state });
+  
+      return res.status(201).json({ msg: "operacion exitosa", result: video });
+    } catch (error) {
+      return res.status(500).json({ message: "Error al insertar", error });
+    }
+  };
+
